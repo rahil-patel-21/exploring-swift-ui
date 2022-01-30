b67a49f18630aea3b6af276e014ec0fcfb95a4a6
TechAddict <rahil.geek21@outlook.com>
2022-01-30 14:42:07 +0530

    Initial Commit

---
+++ exploring-swift-ui/exploring_swift_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  exploring_swift_uiApp.swift
+//  exploring-swift-ui
+//
+//  Created by TechAddict on 30/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct exploring_swift_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
